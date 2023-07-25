ac387fc1ed6a2b5accb742a475bd6d8faa1a3bfb
yeonsu <drawingbonny@gmail.com>
2023-07-25 23:18:30 +0900

    [Swift] Protocol(2)

--- Swift/Protocol.playground/Contents.swift
+++ Swift/Protocol.playground/Contents.swift
@@ -43,3 +43,54 @@ myTV.id
 myTV.name
 myTV.name = "Jay "
 myTV.name
+
+
+
+
+/** 2. 메서드 요구사항 정의
+ 
+  * 메서드의 헤드부분을 정의
+  * mutating, static, class 키워드의 사용법
+ 
+  * 생성자 요구사항
+  * 클래스에서 생성자 채택할 경우 required 키워드 필수(final로 선언 시 생략 가능)
+  * 지정 생성자 / 편의 생성자로 구현 가능
+  * init? -> init / init? / init! 으로 구현 가능
+ 
+**/
+
+
+protocol Toggled {
+    mutating func toggle()
+}
+
+struct SwitchSetting {
+    var isOn = true
+    
+    mutating func toggle() {
+        isOn = isOn ? false : true
+    }
+}
+
+var settingSwitchButton = SwitchSetting()
+
+settingSwitchButton.isOn    // true
+settingSwitchButton.toggle()
+settingSwitchButton.isOn    // false
+
+
+
+protocol DataList {
+    subscript(index: Int) -> Int { get }
+}
+
+struct DataStructure: DataList {
+    subscript(index: Int) -> Int {
+        get {
+            return 0
+        }
+        set {
+            
+        }
+    }
+}
